4ad1843e80bba6a2b8196a63887fe34d3032f0d7
DonAntonio04 <berrellezantonio04@gmail.com>
2023-11-22 23:23:18 -0700

    Primero

--- Ejercicios_Lista_profe/Listas.cs
+++ Ejercicios_Lista_profe/Listas.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Ejercicios_Lista_profe
 {
-    //Lista Enlazada simple
+  //  Lista Enlazada simple
     public class Listas
     {
         Nodo primero;
@@ -20,27 +20,27 @@ namespace Ejercicios_Lista_profe
 
         public bool ListaVacia()
         {
-           if (primero == null)
+            if (primero == null)
             {
                 return true;
             }
-           return false;
+            return false;
 
-           // return primero == null ? true : false;  <--Esto es lo mismo pero en una sola linea
+            // return primero == null ? true : false;  <--Esto es lo mismo pero en una sola linea
         }
 
         public int LongitudLista()
         {
             int contador = 0;
-            if(ListaVacia())
+            if (ListaVacia())
             {
                 return contador;
             }
             else
             {
-                if(primero != null && primero.getSiguiente() != null)
+                if (primero != null && primero.getSiguiente() != null)
                 {
-                    return (contador+1);
+                    return (contador + 1);
                 }
                 else
                 {
@@ -63,7 +63,7 @@ namespace Ejercicios_Lista_profe
             }
             else
             {
-                Nodo actual = primero; 
+                Nodo actual = primero;
                 Console.WriteLine(primero.getDatos());
                 while (actual.getSiguiente != null)
                 {
@@ -71,7 +71,7 @@ namespace Ejercicios_Lista_profe
                     actual = actual.getSiguiente();
                 }
                 Console.WriteLine("--> null");
-               
+
             }
         }
 
@@ -87,7 +87,7 @@ namespace Ejercicios_Lista_profe
                 Nodo actual = primero;
                 while (actual != null)
                 {
-                    if(actual.getDatos() == num)
+                    if (actual.getDatos() == num)
                     {
                         Console.WriteLine("Si se encuentra el elemento deseado");
                         encontrado = true;
@@ -104,11 +104,100 @@ namespace Ejercicios_Lista_profe
             }
         }
 
-        public void BuscarPosicion()
+        public int ObtenerPosicion(int elemento)
         {
+            int posicion = 0;
+            bool encontrado = false;
 
+            if (ListaVacia())
+            {
+                Console.WriteLine("La lista está vacía");
+            }
+            else
+            {
+                Nodo actual = primero;
+
+                while (actual != null) // recorrer los nodos de la lista
+                {
+                    if (actual.getDatos() == elemento) //  nodo actual es igual al elemento buscado
+                    {
+                        encontrado = true;
+                        break;
+                    }
+                    else
+                    {
+                        actual = actual.getSiguiente(); // pasamos al siguiente nodo
+                        posicion++;
+                    }
+                }
+
+                if (encontrado) // si se encuentra el elemento 
+                {
+                    return posicion; //devuelvo la poicion
+                }
+                else
+                {
+                    Console.WriteLine("El elemento no se encuentra en la lista");
+                }
+            }
+
+            return -1;
         }
+
+        public void InsertarElemento()
+        {
+            Console.WriteLine("Ingrese el nuevo elemento:");
+            int nuevoDato = int.Parse(Console.ReadLine());
+
+            Nodo nuevoNodo = new Nodo(nuevoDato); // creo un nuevo nodo
+
+            if (ListaVacia())
+            {
+                primero = nuevoNodo;
+            }
+            else
+            {
+                Nodo actual = primero;
+
+                while (actual.getSiguiente() != null)
+                {
+                    actual = actual.getSiguiente(); // mover el último nodo de la lista
+                }
+
+
+            }
+
+            Console.WriteLine("Elemento insertado correctamente.");
+        }
+        public void InsertarElementoFinal()
+        {
+            Console.WriteLine("Ingrese el nuevo elemento:");
+            int nuevoDato = int.Parse(Console.ReadLine());
+
+            Nodo nuevoNodo = new Nodo(nuevoDato); // creo un nuevo nodo
+
+            if (ListaVacia())
+            {
+                primero = nuevoNodo;
+                ultimo = nuevoNodo; // Cuando la lista está vacía, el nuevo nodo es tanto el primero como el último
+            }
+            else
+            {
+                // En lugar de usar setSiguiente, asignamos directamente el siguiente nodo
+                ultimo = nuevoNodo;
+
+                // El nuevo nodo ahora es el último nodo de la lista
+            }
+
+            Console.WriteLine("Elemento insertado correctamente.");
+        }
+
+
 
 
     }
 }
+
+
+
+
--- Ejercicios_Lista_profe/Nodo.cs
+++ Ejercicios_Lista_profe/Nodo.cs
@@ -3,34 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Ejercicios_Lista_profe
 {
-    public class Nodo
+    //public class Nodo
+    //{
+    //    private int _datos;
+    //    private Nodo _Siguiente;
+    //    public Nodo(int valor)
+    //    {
+    //        _datos = valor;
+    //        _Siguiente = null;
+    //    }
+
+    //    public Nodo(int valor, Nodo siguiente)
+    //    {
+    //        _datos = valor;
+    //        _Siguiente = siguiente;
+    //    }
+    //    //public int Datos { get; }
+    //    //public Nodo Siguiente { get; }
+
+    //    public int getDatos()
+    //    {
+    //        return _datos;
+    //    }
+    //    public Nodo getSiguiente()
+    //    {
+    //        return _Siguiente;
+    //    }
+    //}
+    private int Persona _datos;
+    private Nodo _Siguiente;
+
+    public Nodo(Persona datos)
     {
-        private int _datos;
-        private Nodo _Siguiente;
-        public Nodo(int valor) 
-        {
-            _datos = valor;
-            _Siguiente = null;
-        }
+         = valor;
+        siguiente = null;
+    }
 
-        public Nodo(int valor, Nodo siguiente)
-        {
-            _datos = valor;
-            _Siguiente = siguiente;
-        }
-        //public int Datos { get; }
-        //public Nodo Siguiente { get; }
+    public Persona getDatos()
+    {
+        return datos;
+    }
 
-        public int getDatos()
-        {
-            return _datos;
-        }
-        public Nodo getSiguiente()
-        {
-            return _Siguiente;
-        }
+    public Nodo getSiguiente()
+    {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente)
+    {
+        this.siguiente = siguiente;
     }
 }
